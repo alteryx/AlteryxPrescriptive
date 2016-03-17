17d743cd52f36a2b1c5b45c4cf7ada4d464671fc
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-03-17 14:56:30 -0700

    fix qp_complex.lp to make all tests pass!

--- tests/testthat/test-fileInput-QP.R
+++ tests/testthat/test-fileInput-QP.R
@@ -12,6 +12,7 @@ config <- list(
 #payload = append(config, list(idata = NULL))
 payload = list(config = config, inputs = NULL)
 
+
 test_that('Quadprog solves Simple QP', {
   payload$config$filePath = getSampleData('simple_qp.lp')
   sol <- AlteryxSolve(payload)
@@ -35,15 +36,16 @@ test_that('Gurobi solves Complex QP', {
   payload$config$filePath = getSampleData('qp_complex.lp')
   payload$config$solver = 'gurobi'
   res <- capture.output(sol <- AlteryxSolve(payload))
-  expect_equal(sol$objval, 3)
+  expect_that(abs(sol$objval-2.1111), is_less_than(0.0001))
 })
 
 
-# test_that('Quadprog errors solving Comples QP', {
-#   payload$config$filePath = getSampleData('simple_qp.lp')
-#   payload$config$solver = 'quadprog'
-#   expect_error(AlteryxSolve(payload))
-# })
+test_that('Quadprog errors solving Comples QP', {
+  payload$config$filePath = getSampleData('qp_complex.lp')
+  payload$config$solver = 'quadprog'
+  sol <- AlteryxSolve(payload)
+  expect_that(abs(sol$objval-2.1111), is_less_than(0.0001))
+})
 
 
 # test_that('Quadprog solves QP with three terms', {
