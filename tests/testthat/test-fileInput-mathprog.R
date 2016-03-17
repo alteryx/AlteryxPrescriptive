363a9c868d23a2f1b28ab181863e2e0773ada710
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-03-17 15:52:24 -0700

    add test for mathprog file

---
+++ tests/testthat/test-fileInput-mathprog.R
@@ -0,0 +1,30 @@
+context("fileInput-MPS")
+
+config <- list(
+  inputMode = "file",
+  fileType = "MathProg",
+  problemType = "lp",
+  maximize = TRUE,
+  solver = "glpk"
+)
+
+
+payload <- list(config = config, inputs = NULL)
+
+
+test_that("Cell Tower LP is solved correctly by glpk", {
+  payload$config$filePath = getSampleData("lpTwoVars.lp")
+  payload$config$solver = 'glpk'
+  sol = AlteryxSolve(payload)
+  expect_equal(sol$objval, 34)
+})
+
+
+test_that("Cell Tower LP is solved correctly by gurobi", {
+  skip_on_travis()
+  library(gurobi)
+  payload$config$filePath = getSampleData("lpTwoVars.lp")
+  payload$config$solver = 'gurobi'
+  out = capture.output(sol <- AlteryxSolve(payload))
+  expect_equal(sol$objval, 34)
+})
