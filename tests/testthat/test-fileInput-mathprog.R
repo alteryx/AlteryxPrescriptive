3d299f34b51cbf68acd8a235d6b00a173bb5a8ee
Chris Freeman <cfreeman.code@gmail.com>
2016-03-18 14:17:08 -0500

    restored the tests for `.mod` files

--- tests/testthat/test-fileInput-mathprog.R
+++ tests/testthat/test-fileInput-mathprog.R
@@ -13,7 +13,7 @@ payload <- list(config = config, inputs = NULL)
 
 
 test_that("Cell Tower LP is solved correctly by glpk", {
-  payload$config$filePath = getSampleData("lpTwoVars.lp")
+  payload$config$filePath = getSampleData("lpTwoVars.mod")
   payload$config$solver = 'glpk'
   sol = AlteryxSolve(payload)
   expect_equal(sol$objval, 34)
@@ -23,7 +23,7 @@ test_that("Cell Tower LP is solved correctly by glpk", {
 test_that("Cell Tower LP is solved correctly by gurobi", {
   skip_on_travis()
   library(gurobi)
-  payload$config$filePath = getSampleData("lpTwoVars.lp")
+  payload$config$filePath = getSampleData("lpTwoVars.mod")
   payload$config$solver = 'gurobi'
   out = capture.output(sol <- AlteryxSolve(payload))
   expect_equal(sol$objval, 34)
