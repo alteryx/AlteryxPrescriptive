8d6359ecbdf3c365fd728543b6d1eb2a3897f4a1
Kuo Liu <kliu@alteryx.com>
2016-04-04 11:02:48 -0600

    Add getBounds_glpkAPI to convert bounds in ROI structure to glpkAPI structure

--- R/getBounds.R
+++ R/getBounds.R
@@ -34,7 +34,8 @@ getBounds <- function(mod){
   return(bounds)
 }
 
-# Get bounds for Gurobi
+# Get bounds for Gurobi ----
+# mod: OP object from ROI
 getBounds_gurobi <- function(mod){
   li <- mod$bounds$lower$ind
   ui <- mod$bounds$upper$ind
@@ -56,4 +57,44 @@ getBounds_gurobi <- function(mod){
     }
     list(lb = lb_, ub = ub_)
   }
+}
+
+# Get bounds for glpkAPI
+# mod: list, lp$bounds where lp is OP object from ROI
+getBounds_glpkAPI <- function(mod, n_objective_vars) {
+  lb <- rep(0, n_objective_vars)
+  up <- rep(Inf, n_objective_vars)
+
+  ind_lb <- mod$bounds$lower$ind
+  val_lb <- mod$bounds$lower$val
+  ind_ub <- mod$bounds$upper$ind
+  val_ub <- mod$bounds$upper$val
+
+  lb[ind_lb] <- val_lb
+  up[ind_ub] <- val_ub
+
+  type <- rep(0, n_objective_vars)
+  for (i in 1:n_objective_vars) {
+    if (lb[i] == -Inf) {
+      if (ub[i] == Inf) {
+        type[i] <- GLP_FR
+      } else {
+        type[i] <- GLP_UP
+      }
+    } else {
+      if (ub[i] == Inf) {
+        type[i] <- GLP_LO
+      } else {
+        if(ub[i] > lb[i]) {
+          type[i] <- GLP_DB
+        } else if(ub[i] == lb[i]) {
+          type[i] <- GLP_FX
+        } else {
+          stop("Error: decision variable's upper bound is less than its lower bound!")
+        }
+      }
+    }
+  }
+
+  return(list(lb = lb, ub = ub, type = type))
 }
