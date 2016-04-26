bb544690fd2e043a23f5a6b98d45a93aebf3cf1b
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-04-25 21:21:17 -0700

    fix bugs in makeCPLEXFile.
    1. fieldInfo was renamed to fieldList in the json.
    2. general type is coded as General and not Integer.

--- R/manualInputs.R
+++ R/manualInputs.R
@@ -6,16 +6,16 @@
 makeCPLEXFile <- function(x){
   tpl <- "%s\n  obj: %s\nSubject To\n  %s\nBounds\n  %s\nGeneral\n  %s\nBinary\n  %s\nEnd\n"
   ptype = if (x$maximize) 'Maximize' else 'Minimize'
-  bounds = paste(sapply(x$fieldInfo, function(x){
+  bounds = paste(sapply(x$fieldList, function(x){
     x$upperBound = if (is.null(x$upperBound)) "+inf" else x$upperBound
     sprintf("%s <= %s <= %s", x$lowerBound, x$fieldName, x$upperBound)
   }), collapse = "\n  ")
   constraints = paste(x$constraints, collapse = "\n  ")
   find_type <- function(type){
-    f <- Filter(function(x){x$type == type}, x$fieldInfo)
+    f <- Filter(function(x){x$type == type}, x$fieldList)
     paste(sapply(f, '[[', 'fieldName'), collapse = '\n  ')
   }
-  general = find_type('Integer')
+  general = find_type('General')
   binary = find_type('Binary')
   sprintf(tpl, ptype, x$objective, constraints, bounds, general, binary)
 }
