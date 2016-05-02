937e8d5203f8fada3a551da56350d1e6e01d9e2c
kuol <liukuo99@gmail.com>
2016-05-02 13:27:44 -0600

    Test row_activity when returning sensitivity.

--- tests/testthat/test-fileInput-LP-sensitivity.R
+++ tests/testthat/test-fileInput-LP-sensitivity.R
@@ -5,15 +5,19 @@ config <- list(
   inputMode = "file",
   fileType = "CPLEX_LP",
   filePath = getSampleData("table_chair.lp"),
+  solver = "glpkAPI",
   returnSensitivity = TRUE
 )
 
 payload <- list(config = config, inputs = NULL)
+sol <- AlteryxSolve(payload)
 
-test_that("LP sensitivity, file mode (dense), with glpk", {
-  payload$config$solver <- 'glpkAPI'
-  sol <- AlteryxSolve(payload)
+test_that("LP sensitivity, file mode with glpkAPI", {
   df_sensitivity <- sol$sensitivity
   expect_equal(dim(df_sensitivity$constraintsRHS), c(4,10))
 })
 
+test_that("LP row_activity, file mode with glpkAPI", {
+  expect_equal(sol$row_activity$optimals, c(240,100))
+  expect_equal(sol$row_activity$slacks, c(0,0))
+})
