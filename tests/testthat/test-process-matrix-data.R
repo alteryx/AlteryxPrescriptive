288fa98ed9f4ead60a379973e75bacfaec455c11
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-11 18:16:35 -0700

    more tests!!!

--- tests/testthat/test-process-matrix-data.R
+++ tests/testthat/test-process-matrix-data.R
@@ -12,16 +12,23 @@ readInputsFromXL <- function(path){
   library(readxl)
   sheets <- excel_sheets(path)
   inputs <- setNames(lapply(sheets, read_excel, path = path), sheets)
+  inputs <- lapply(inputs, function(input){
+    input[is.na(input)] <- 0
+    return(input)
+  })
 }
 
 library(testthat)
-testFactory <- function(paths, solver = 'glpk', config){
+testFactory <- function(paths, solver = 'glpk', config, sol){
   config$solver <- solver
   results <- lapply(paths, function(path){
     test_that(paste('Testing ', path), {
+      if (grepl("min", basename(path))){
+        config$maximize = FALSE
+      }
       x <- list(config = config, inputs = readInputsFromXL(path))
       out <- AlteryxSolve(x)
-      expect_equal(out$objval, 25000)
+      expect_equal(out$objval, sol[[path]])
     })
   })
 }
@@ -31,4 +38,5 @@ f <- list.files(
   pattern = '.xlsx',
   full.names = TRUE
 )
-testFactory(f, solver = 'glpk', config)
+sol <- setNames(c(rep(25000, 3), 25000,3200), f)
+testFactory(f, solver = 'glpk', config, sol)
