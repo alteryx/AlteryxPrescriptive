f761afeea68da7eba156664a120c73dd0ea2c070
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-06-03 18:43:47 -0700

    update checkInputs to pass R CMD Check.

--- R/assertions.R
+++ R/assertions.R
@@ -1,6 +1,8 @@
 #' Check input quality
 #'
 #'
+#' @param x object to check
+#' @param ... additional parameters
 #' @export
 #' @import assertthat
 checkInputs <- function(x, ...){
@@ -8,6 +10,11 @@ checkInputs <- function(x, ...){
 }
 
 
+#' Check inputs for matrix mode
+#'
+#'
+#' @param x object to check
+#' @param ... additional parameters
 #' @export
 checkInputs_matrix_mode <- function(x, ...){
   assert_that(
@@ -31,7 +38,11 @@ checkInputs_matrix_mode <- function(x, ...){
   are_equal(x$A$nrow, NROW(x$B))
 }
 
-
+#' Check inputs for file mode
+#'
+#'
+#' @param x object to check
+#' @param ... additional parameters
 #' @export
 checkInputs_file_mode <- function(x, ...){
   fileSelected = x$config$filePath
@@ -41,6 +52,11 @@ checkInputs_file_mode <- function(x, ...){
 }
 
 
+#' Check inputs for manual mode
+#'
+#'
+#' @param x object to check
+#' @param ... additional parameters
 #' @export
 checkInputs_manual_mode <- function(x, ...){
   config <- x$config
