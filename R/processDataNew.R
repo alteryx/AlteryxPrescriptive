06b5352da3bff928bca2a31c6668830e06a722f6
kuol <liukuo99@gmail.com>
2016-10-31 14:57:54 -0600

    Allow "constraints in row" without "constraint" column.

--- R/processDataNew.R
+++ R/processDataNew.R
@@ -122,9 +122,10 @@ inferA <- function(A, constrMode) {
   if (constrMode == 'conInRow') {
     constraint <- names(Filter(function(x){return(!isTRUE(x))},
                                sapply(A, is.numeric)))
-    if (is.null(constraint) || length(constraint) == 0) {
-      stop("Error: lack of constraint column in Input A.")
-    } else if (length(constraint) > 1) {
+    # if (is.null(constraint) || length(constraint) == 0) {
+    #   stop("Error: lack of constraint column in Input A.")
+    # } else
+    if (length(constraint) > 1) {
       stop("Error: there shouldn't be any other string type of columns except constraint.")
     }
     names(A)[names(A) == constraint] <- 'constraint'
